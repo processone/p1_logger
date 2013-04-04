7d3d172d6b7195438a0e641967920fe941c0d1de
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2013-04-05 00:20:27 +1000

    Fix module names

--- include/logger.hrl
+++ include/logger.hrl
@@ -21,20 +21,16 @@
 -define(PRINT(Format, Args), io:format(Format, Args)).
 
 -define(DEBUG(Format, Args),
-	ejabberd_logger:debug_msg(?MODULE, ?LINE, Format,
-				  Args)).
+	logger:debug_msg(?MODULE, ?LINE, Format, Args)).
 
 -define(INFO_MSG(Format, Args),
-	ejabberd_logger:info_msg(?MODULE, ?LINE, Format, Args)).
+	logger:info_msg(?MODULE, ?LINE, Format, Args)).
 
 -define(WARNING_MSG(Format, Args),
-	ejabberd_logger:warning_msg(?MODULE, ?LINE, Format,
-				    Args)).
+	logger:warning_msg(?MODULE, ?LINE, Format, Args)).
 
 -define(ERROR_MSG(Format, Args),
-	ejabberd_logger:error_msg(?MODULE, ?LINE, Format,
-				  Args)).
+	logger:error_msg(?MODULE, ?LINE, Format, Args)).
 
 -define(CRITICAL_MSG(Format, Args),
-	ejabberd_logger:critical_msg(?MODULE, ?LINE, Format,
-				     Args)).
+	logger:critical_msg(?MODULE, ?LINE, Format, Args)).
